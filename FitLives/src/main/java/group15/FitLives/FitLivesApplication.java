74af2125ce70d47105ef566abbc0f790b34029ee
boraulukan <81370060+boraulukan@users.noreply.github.com>
2021-03-25 14:58:16 +0000

    first commit

---
+++ FitLives/src/main/java/group15/FitLives/FitLivesApplication.java
@@ -0,0 +1,13 @@
+package group15.FitLives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitLivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitLivesApplication.class, args);
+	}
+
+}
---
+++ FitLives/src/test/java/group15/FitLives/FitLivesApplicationTests.java
@@ -0,0 +1,13 @@
+package group15.FitLives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
